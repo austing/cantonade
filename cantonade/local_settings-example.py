7980d44acde21a351f0e4488f3b04e2affc5777c
Austin Gross <austing@gmail.com>
2014-04-06 18:33:51 +0200

    updated local settings example

--- cantonade/local_settings-example.py
+++ cantonade/local_settings-example.py
@@ -18,7 +18,7 @@ STATIC_URL = '/static/'
 SITE_ID = 1
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/Users/austin/Sites/cantonade/cantonade/librairie/media/'
+MEDIA_ROOT = '/Users/austin/Sites/lacantonade/cantonade/librairie/media/'
 
 
 DATABASES = {
